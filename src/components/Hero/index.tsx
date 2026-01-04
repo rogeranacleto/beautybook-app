aa146f8969cda500eb9484b8d2725211fb31fddd
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-01-04 19:54:47 -0300

    feat: new layout in hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,28 +5,30 @@ import { FaArrowRight } from "react-icons/fa6";
 export function Hero() {
   return (
     <div
-      className="relative min-h-screen bg-cover max-w-350 w-full"
+      className="relative min-h-screen bg-cover max-w-screen w-full bg-black"
       style={{
         backgroundImage: `url(${bgHero})`,
-        backgroundPosition: "center 10%",
+        backgroundPosition: "center 5%",
+        backgroundSize: "2000px",
+        backgroundRepeat: "no-repeat"
       }}
     >
-      <div className="absolute inset-0 bg-black/50"></div>
+      <div className="absolute inset-0 bg-black/70"></div>
       <div className="relative z-10 text-white">
         <header>
           <Header />
         </header>
-        <div className="flex flex-col items-center mt-20">
+        <div className="flex flex-col items-center mt-50">
           <p className="text-xs bg-[#0A0A0A] px-4 py-1 rounded-3xl border border-solid border-[#646161] flex items-center gap-2">
             <div className="h-2 w-2 bg-white rounded-full"></div>LOREM IPSUM
             LOREM
           </p>
-          <p className="text-6xl font-medium mt-3 bg-linear-to-r from-white to-pink-800 bg-clip-text text-transparent pb-6">
-            BeautyBook
+          <p className="text-6xl max-w-3xl text-center font-medium mt-3 bg-linear-to-r from-white to-pink-800 bg-clip-text text-transparent pb-6">
+            Beautybook agendamentos simples
           </p>
-          <p className="text-center">
+          <p className="text-center max-w-3xl">
             Lorem ipsum molestias illum eligendi cupiditate <br /> fuga sed
-            alias, possimus quasi nemo dicta fuga sed alias quas.
+            alias, possimus quasi nemo dicta fuga sed alias quas. Lorem ipsum tetur, aperiam accusamus deserunt, et laboriosam, in repellat eum culpa enim dolores ea vitae.
           </p>
           <button className="text-xs bg-white text-black px-8 py-4 flex items-center gap-3 rounded-3xl hover:scale-110 duration-200 ease-in-out mt-8 cursor-pointer">
             ACESSAR <FaArrowRight className="text-xs" />
