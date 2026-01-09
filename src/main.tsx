084f0cf1b9b959623a407cae1442a62ee7363dd0
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-01-09 17:29:18 -0300

    feat: set up application entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,41 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { router } from './App'
 import { RouterProvider } from 'react-router'
-createRoot(document.getElementById('root')!).render(
+import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './contexts/AuthContext'
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <AuthProvider>
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false}
+        gutter={10}
+        containerStyle={{
+          padding: "10",
+        }}
+        toasterId="default"
+        toastOptions={{
+          className: "",
+          duration: 5000,
+          removeDelay: 1000,
+          style: {
+            border: "1px solid #000",
+            background: "#000",
+            color: "#fff",
+            borderRadius: "10px",
+            padding: "16px 25px",
+            fontFamily: "sans-serif",
+          },
+          success: {
+            duration: 5000,
+            iconTheme: {
+              primary: "green",
+              secondary: "white",
+            },
+          },
+        }}
+      />
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
