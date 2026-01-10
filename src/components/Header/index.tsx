3f786c9ea4148949def1662affb28952a73f214a
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-01-10 16:05:19 -0300

    style: add animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,34 +10,48 @@ export function Header() {
     <div className="max-w-screen bg-transparent w-full">
       <nav className="flex flex-col gap-4 p-6 md:flex-row md:items-center md:justify-between md:p-8">
         <div className="flex gap-4 justify-center md:justify-start">
-          <button
-            className="text-sm hover:text-[#f0a] duration-300 ease-in-out cursor-pointer"
-            onClick={() => scrollToSection("platform")}
-          >
-            Plataforma
-          </button>
-          <button
-            className="text-sm hover:text-[#f0a] duration-300 ease-in-out cursor-pointer"
-            onClick={() => scrollToSection("mission")}
-          >
-            Missão
-          </button>
+          <div data-aos="fade-right" data-aos-duration="3000">
+            <button
+              className="text-sm hover:text-[#f0a] duration-300 ease-in-out cursor-pointer"
+              onClick={() => scrollToSection("platform")}
+              data-aos="fade-right"
+              data-aos-duration="3000"
+            >
+              Plataforma
+            </button>
+          </div>
+          <div data-aos="fade-right" data-aos-duration="3000">
+            <button
+              className="text-sm hover:text-[#f0a] duration-300 ease-in-out cursor-pointer"
+              onClick={() => scrollToSection("mission")}
+            >
+              Missão
+            </button>
+          </div>
         </div>
-        <div className="text-center">
+        <div
+          className="text-center"
+          data-aos="fade-up"
+          data-aos-duration="3000"
+        >
           <h1 className="font-medium text-lg">BeautyBook</h1>
         </div>
         <div className="flex gap-4 justify-center md:justify-end items-center">
-          <Link to="/login">
-            <p className="text-sm hover:text-[#f0a] duration-300 ease-in-out cursor-pointer">
-              Entrar
-            </p>
-          </Link>
+          <div data-aos="fade-left" data-aos-duration="3000">
+            <Link to="/login">
+              <p className="text-sm hover:text-[#f0a] duration-300 ease-in-out cursor-pointer">
+                Entrar
+              </p>
+            </Link>
+          </div>
 
-          <Link to="/signup">
-            <p className="text-sm bg-[#0A0A0A] border border-solid border-[#646161] px-4 py-2 rounded-3xl cursor-pointer hover:bg-[#f0a] duration-300 ease-in-out hover:border-black">
-              Cadastro
-            </p>
-          </Link>
+          <div data-aos="fade-left" data-aos-duration="3000">
+            <Link to="/signup">
+              <p className="text-sm bg-[#0A0A0A] border border-solid border-[#646161] px-4 py-2 rounded-3xl cursor-pointer hover:bg-[#f0a] duration-300 ease-in-out hover:border-black">
+                Cadastro
+              </p>
+            </Link>
+          </div>
         </div>
       </nav>
     </div>
