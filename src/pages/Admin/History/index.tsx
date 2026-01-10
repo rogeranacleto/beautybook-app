bcbcab013dc542c6bef7aeec2c8173ad3e9bfbcc
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-01-10 14:36:25 -0300

    feat: add history page for finished appointments

---
+++ src/pages/Admin/History/index.tsx
@@ -0,0 +1,8 @@
+import { HistoryTable } from "../../../components/HistoryTable";
+export function History() {
+  return (
+    <div className="bg-[#030303] max-w-screen min-h-screen rounded-2xl p-7">
+      <HistoryTable/>
+    </div>
+  );
+}
