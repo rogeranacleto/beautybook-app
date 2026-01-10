2627150aea431677e3eda5776d35d4550a35f130
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-01-10 14:37:23 -0300

    change image and border of cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { Hero } from "../../components/Hero"
 import { MdCheckCircleOutline } from "react-icons/md";
-import bgBeautyBook from "../../assets/bg-beautybook.png";
+import imgBeautyBook from "../../assets/img-beautybook.jpg";
 import { Footer } from "../../components/Footer";
 import { MdOutlinePerson } from "react-icons/md";
 import { LuLayoutDashboard } from "react-icons/lu";
@@ -19,7 +19,7 @@ export function Home(){
               Pilares essenciais para um agendamento simples
             </h1>
             <div className="flex gap-5 flex-wrap">
-              <div className="max-w-sm bg-black rounded-lg p-10 h-80 hover:scale-105 duration-300 ease-in-out border border-solid border-[#f0a]">
+              <div className="max-w-sm bg-black rounded-lg p-10 h-80 hover:scale-105 duration-300 ease-in-out border border-solid border-[#a1999918]">
                 <div className="bg-[#000000] border border-solid border-[#f0a] w-10 h-10 flex items-center justify-center rounded-lg">
                   <LuLayoutDashboard className="text-2xl text-[#f0a]" />
                 </div>
@@ -31,7 +31,7 @@ export function Home(){
                   para decisões mais rápidas.
                 </p>
               </div>
-              <div className="max-w-sm bg-black rounded-lg p-10 h-80 hover:scale-105 duration-300 ease-in-out border border-solid border-[#f0a]">
+              <div className="max-w-sm bg-black rounded-lg p-10 h-80 hover:scale-105 duration-300 ease-in-out border border-solid border-[#a1999918]">
                 <div className="bg-[#000000] border border-solid border-[#f0a] w-10 h-10 flex items-center justify-center rounded-lg">
                   <MdOutlinePerson className="text-2xl text-[#f0a]" />
                 </div>
@@ -43,7 +43,7 @@ export function Home(){
                   inativos para ter mais controle do seu cadastro.
                 </p>
               </div>
-              <div className="max-w-sm bg-black rounded-lg p-10 h-80 hover:scale-105 duration-300 ease-in-out border border-solid border-[#f0a]">
+              <div className="max-w-sm bg-black rounded-lg p-10 h-80 hover:scale-105 duration-300 ease-in-out border border-solid border-[#a1999918]">
                 <div className="bg-[#000000] border border-solid border-[#f0a] w-10 h-10 flex items-center justify-center rounded-lg">
                   <HiOutlineClipboardCheck className="text-2xl text-[#f0a]" />
                 </div>
@@ -70,13 +70,13 @@ export function Home(){
                 A plataforma é 100% grátis, utilize sem nenhum custo
               </h1>
               <img
-                src={bgBeautyBook}
+                src={imgBeautyBook}
                 alt="Beauty Book Background"
-                className="h-147 rounded-2xl"
+                className="h-147 rounded-2xl border border-solid border-[#a1999918]"
               />
             </div>
             <div className="mt-1 flex flex-col gap-5">
-              <div className="mt-37 hover:scale-105 duration-300 ease-in-out bg-black text-white max-w-lg border border-solid border-[#a199995e] p-6 rounded-lg">
+              <div className="mt-37 hover:scale-105 duration-300 ease-in-out bg-black text-white max-w-lg border border-solid border-[#a1999918] p-6 rounded-lg">
                 <div className="flex items-center gap-2 mb-2">
                   <MdCheckCircleOutline className="text-lg text-[#f0a] font-bold" />
                   <p className="text-[13px] text-[#f0a] font-bold">
@@ -84,13 +84,13 @@ export function Home(){
                   </p>
                 </div>
                 <p className="text-lg mb-2 font-medium">Controle completo</p>
-                <p className="text-sm text-[#a19999] mb-2">
+                <p className="text-sm text-gray-400 mb-2">
                   Visualize clientes, procedimentos em andamento e finalizados
                   rapidamente em um só lugar.
                 </p>
-                <p className="text-[#a19999] text-sm">R$0,00</p>
+                <p className="text-gray-400 text-sm">R$0,00</p>
               </div>
-              <div className="hover:scale-105 duration-300 ease-in-out bg-black text-white max-w-lg border border-solid border-[#a199995e] p-6 rounded-lg">
+              <div className="hover:scale-105 duration-300 ease-in-out bg-black text-white max-w-lg border border-solid border-[#a1999918] p-6 rounded-lg">
                 <div className="flex items-center gap-2 mb-2">
                   <MdCheckCircleOutline className="text-lg text-[#f0a] font-bold" />
                   <p className="text-[13px] text-[#f0a] font-bold">CONTROLE</p>
@@ -98,13 +98,13 @@ export function Home(){
                 <p className="text-lg mb-2 font-medium">
                   Clientes centralizados
                 </p>
-                <p className="text-sm text-[#a19999] mb-2">
+                <p className="text-sm text-gray-400 mb-2">
                   Cadastre clientes rapidamente, organize informações pessoais,
                   ative ou inative registros facilmente.
                 </p>
-                <p className="text-[#a19999] text-sm">R$0,00</p>
+                <p className="text-gray-400 text-sm">R$0,00</p>
               </div>
-              <div className="hover:scale-105 duration-300 ease-in-out bg-black text-white max-w-lg border border-solid border-[#a199995e] p-6 rounded-lg">
+              <div className="hover:scale-105 duration-300 ease-in-out bg-black text-white max-w-lg border border-solid border-[#a1999918] p-6 rounded-lg">
                 <div className="flex items-center gap-2 mb-2">
                   <MdCheckCircleOutline className="text-lg text-[#f0a] font-bold" />
                   <p className="text-[13px] text-[#f0a] font-bold">
@@ -114,11 +114,11 @@ export function Home(){
                 <p className="text-lg mb-2 font-medium">
                   Controle de atendimentos
                 </p>
-                <p className="text-sm text-[#a19999] mb-2">
+                <p className="text-sm text-gray-400 mb-2">
                   Registre procedimentos, acompanhe atendimentos em andamento,
                   finalize serviços e consulte históricos facilmente.
                 </p>
-                <p className="text-[#a19999] text-sm">R$0,00</p>
+                <p className="text-gray-400 text-sm">R$0,00</p>
               </div>
             </div>
           </div>
