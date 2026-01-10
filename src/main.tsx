54f1a47c7fdbe32d01628a109bc3cb212e26486d
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-01-10 14:06:59 -0300

    feat: add global context providers and toast configuration

--- src/main.tsx
+++ src/main.tsx
@@ -5,39 +5,51 @@ import { router } from './App'
 import { RouterProvider } from 'react-router'
 import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from './contexts/AuthContext'
+import { ClientsUpdateProvider } from './contexts/ClientsUpdateContext'
+import { ClientsCardContextProvider } from './contexts/ClientsCardContext'
+import { AppointmentsUpdateProvider } from './contexts/AppointmentsUpdateContext'
+import { AppointmentsCardContextProvider } from './contexts/AppointmentsCardContext'
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <Toaster
-        position="bottom-right"
-        reverseOrder={false}
-        gutter={10}
-        containerStyle={{
-          padding: "10",
-        }}
-        toasterId="default"
-        toastOptions={{
-          className: "",
-          duration: 5000,
-          removeDelay: 1000,
-          style: {
-            border: "1px solid #000",
-            background: "#000",
-            color: "#fff",
-            borderRadius: "10px",
-            padding: "16px 25px",
-            fontFamily: "sans-serif",
-          },
-          success: {
-            duration: 5000,
-            iconTheme: {
-              primary: "green",
-              secondary: "white",
-            },
-          },
-        }}
-      />
-      <RouterProvider router={router} />
+      <ClientsUpdateProvider>
+        <ClientsCardContextProvider>
+          <AppointmentsUpdateProvider>
+            <AppointmentsCardContextProvider>
+              <Toaster
+                position="bottom-right"
+                reverseOrder={false}
+                gutter={10}
+                containerStyle={{
+                  padding: "10",
+                }}
+                toasterId="default"
+                toastOptions={{
+                  className: "",
+                  duration: 5000,
+                  removeDelay: 1000,
+                  style: {
+                    border: "1px solid #000",
+                    background: "#000",
+                    color: "#fff",
+                    borderRadius: "10px",
+                    padding: "16px 25px",
+                    fontFamily: "sans-serif",
+                  },
+                  success: {
+                    duration: 5000,
+                    iconTheme: {
+                      primary: "green",
+                      secondary: "white",
+                    },
+                  },
+                }}
+              />
+              <RouterProvider router={router} />
+            </AppointmentsCardContextProvider>
+          </AppointmentsUpdateProvider>
+        </ClientsCardContextProvider>
+      </ClientsUpdateProvider>
     </AuthProvider>
   </StrictMode>
 );
